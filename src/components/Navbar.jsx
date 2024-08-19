79ed8267311d1800b47b106bc39dd6c1be98ac17
Sukrit Chopra <109043008+nottysukku@users.noreply.github.com>
2024-08-19 18:34:28 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -31,7 +31,7 @@ const Navbar = () => {
       <img
         id="darkm"
         className='cursor-pointer relative right-6'
-        src="src/assets/asd.png"
+        src="src/assets/asd.jpg"
         alt={isInverted ? "darkmode" : "lightmode"}
         onClick={handleClick}
         style={{ filter: isInverted ? 'invert(1)' : 'none' }}
